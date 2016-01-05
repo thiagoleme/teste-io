f40b95028e2ecae71864337133b13f0390d2aa0e
Thiago Leme <leme.thiagoh@gmail.com>
2016-01-05 15:24:03 -0200

    Aula 04 - Exercicio 07 - Escrevendo com BufferedWriter

--- src/br/com/trilha/java/cursoIII/IO/TestaIO.java
+++ src/br/com/trilha/java/cursoIII/IO/TestaIO.java
@@ -1,19 +1,33 @@
 package br.com.trilha.java.cursoIII.IO;
 
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class TestaIO {
 
 	public static void main(String[] args) throws IOException {
-		InputStream is = System.in;
+		InputStream is = new FileInputStream("arquivo.txt");
 		Scanner entrada = new Scanner(is);
+		
+		OutputStream os = new FileOutputStream("saida.txt");
+		OutputStreamWriter osw  = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
 
 		while (entrada.hasNextLine()) {
-            System.out.println(entrada.nextLine());
+			String linha = entrada.nextLine();
+            System.out.println(linha);
+            
+            bw.write(linha);
+            bw.newLine();
         }
 
+		bw.close();
 		entrada.close();
 	}
 
