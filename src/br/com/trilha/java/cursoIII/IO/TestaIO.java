3e58faefd714427fd7ffaf735440e21362690d92
Thiago Leme <leme.thiagoh@gmail.com>
2016-01-05 15:35:58 -0200

    Aula 04 - Exercício 08 - Ler do Console e gravar no arquivo

--- src/br/com/trilha/java/cursoIII/IO/TestaIO.java
+++ src/br/com/trilha/java/cursoIII/IO/TestaIO.java
@@ -1,19 +1,16 @@
 package br.com.trilha.java.cursoIII.IO;
 
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestaIO {
 
 	public static void main(String[] args) throws IOException {
-		InputStream is = new FileInputStream("arquivo.txt");
+		InputStream is = System.in;
 		Scanner entrada = new Scanner(is);
 
 		OutputStream os = new FileOutputStream("saida.txt");
