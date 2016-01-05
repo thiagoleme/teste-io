12180fbc72eeb1f21e0721aa01f4fc741a704005
Thiago Leme <leme.thiagoh@gmail.com>
2016-01-05 15:08:43 -0200

    Aula 04 - até Exercicio 5

--- src/br/com/trilha/java/cursoIII/IO/TestaIO.java
+++ src/br/com/trilha/java/cursoIII/IO/TestaIO.java
@@ -1,9 +1,24 @@
 package br.com.trilha.java.cursoIII.IO;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestaIO {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(
+								new InputStreamReader(
+									new FileInputStream("arquivo.txt")));
+		String s = br.readLine();
+
+		while (s != null) {
+			System.out.println(s);
+			s = br.readLine();
+		}
+
+		br.close();
 	}
 
 }
